38742017916f3b62a80030bfcd8afc0bed12c365
Rufus Daw <156686670+rhdaw@users.noreply.github.com>
2025-12-16 11:23:09 +0000

    Additions to allow for prediciton to be applied to standard data - not test data alone

--- src/2_cycombine/main_first_remove_qc_naming.py
+++ src/2_cycombine/main_first_remove_qc_naming.py
@@ -1,14 +1,74 @@
 from pathlib import Path
 import os
 
-post_ai_path = Path('/Volumes/grainger/Common/stroke_impact_smart_tube/computational_outputs/fcs_files/altered_fcs_files/post_flowai')
+additional_remove = [
+    "A12 Unstained_Mainstain.fcs",
+    "C12 Unstained_Mainstain.fcs",
+    "D12 Unstained_Mainstain.fcs",
+    "A11 Unstained_Mainstain.fcs",
+    "B11 Unstained_Mainstain.fcs",
+    "G10 Unstained_Mainstain.fcs",
+    "G10 Batchcontrol_PolyIC_040_20112024_Mainstain.fcs",
+    "G9 Batchcontrol_LPS_300_20112024_Mainstain.fcs",
+    "H1 Unstained_Mainstain.fcs",
+    "H2 Unstained_Mainstain.fcs",
+    "A5 Batchcontrol3_001_Mainstain.fcs",
+    "A6 Batchcontrol3_IFNa2_Mainstain.fcs",
+    "A8 Batchcontrol_PolyIC_Mainstain.fcs",
+    "H1 BATCHCONTROL_Batch8_001_Mainstain.fcs",
+    "H2 BATCHCONTROL_Batch8_020_Mainstain.fcs",
+    "H3 BATCHCONTROL_Batch8_300_Mainstain.fcs",
+    "H10 Unstained_Mainstain.fcs",
+    "H11 Unstained_Mainstain.fcs",
+    "H12 Unstained_Mainstain.fcs",
+    "H5 Unstained_Mainstain.fcs",
+    "H6 Unstained_Mainstain.fcs",
+    "H7 Unstained_Mainstain.fcs",
+    "H8 Unstained_Mainstain.fcs",
+    "H9 Unstained_Mainstain.fcs",
+    "H1 Batchcontrol_190325_hc3_001_Mainstain.fcs",
+    "H2 Batchcontrol_190325_hc3_020_Mainstain.fcs",
+    "H3 Batchcontrol_190325_hc3_300_Mainstain.fcs",
+    "H4 Batchcontrol13_250325_hc3_001_Mainstain.fcs",
+    "H5 Batchcontrol13_250325_hc3_020_Mainstain.fcs",
+    "H6 Batchcontrol13_250325_hc3_300_Mainstain.fcs",
+    "G5 H5 Batchcontrol25042025_IFN3_Mainstain.fcs",
+    "E4 S-127-InP-020_Mainstain.fcs",
+    "F2 S-117-InP-300_Mainstain.fcs",
+    "E3 S-108-InP-020_Mainstain.fcs",
+    "E1 S-180-6M-020_Mainstain.fcs",
+    "E2 S-180-6M-300_Mainstain.fcs",
+    "D7 S-178-InP-001_Mainstain.fcs",
+    "C5 M-163-InP-001_Mainstain.fcs",
+    "G6 S-244-InP-020_Mainstain.fcs",
+    "E4 S-108-InP-300_Mainstain.fcs",
+    "E6 S-110-InP-020_Mainstain.fcs",
+    "F8 S-171-6M-020_Mainstain.fcs",
+    "E6 S-182-6M-001_Mainstain.fcs",
+    "E9 S-124-InP-020_Mainstain.fcs",
+    "E10 S-124-InP-300_Mainstain.fcs",
+    "E8 S-182-6M-300_Mainstain.fcs",
+    "E7 S-182-6M-020_Mainstain.fcs",
+    "H1 S-194-6M-040_Mainstain.fcs",
+    "E7 S-110-InP-300_Mainstain.fcs",
+    "F7 S-193-InP-001_Mainstain.fcs",
+]
+
+post_ai_path = Path(
+    "/Users/user/Documents/final_stroke_data/flowai_fcs_files_results_qc"
+)
 directory_files = os.listdir(post_ai_path)
 
 for file in directory_files:
-    if '_QC' in file:
-        new_filename = file.replace('_QC', '')
+    if "_QC" in file:
+        new_filename = file.replace("_QC", "")
         old_full_path = os.path.join(post_ai_path, file)
         new_full_path = os.path.join(post_ai_path, new_filename)
-        
+
         os.rename(old_full_path, new_full_path)
-        print(f'Renamed: {file} -> {new_filename}')
+        print(f"Renamed: {file} -> {new_filename}")
+
+    for ar in additional_remove:
+        if ar in file:
+            os.remove(os.path.join(post_ai_path, file))
+            print(f"removed {file} as matches {ar}")
--- src/3_unito/main.py
+++ src/3_unito/main.py
@@ -1,36 +1,37 @@
 from unito_objects import PipelineConfig, UNITOPipeline
 
-def main():
 
+def main():
     config = PipelineConfig(
-        fcs_dir='/Users/user/Documents/UNITO_csv_conversion/fcs_dir_testing/', # Dir of your .fcs files
-        wsp_path='/Users/user/Documents/UNITO_train_wsp/WSP_22052025.wsp', # Location of you .wsp file
-        wsp_files_dir='/Users/user/Documents/UNITO_train_wsp/', # Dir holding the .wsp
-        panel_meta_path='/Volumes/grainger/Common/stroke_impact_smart_tube/computational_outputs/fcs_files/metadata_files/panel_metadata_all_batches.csv', # Path to panel metadatafile needed for cycombine
-        ram_disk= True, # Want to use a RAMDisk (MacOS only)?
-        csv_conversion_dir='/Users/user/Documents/UNITO_csv_conversion/', # Dir for the converted files to go to
-        csv_conversion_dir_metadir='/Users/user/Documents/UNITO_csv_conversion/metadata/', # Dir for the converstedd file metadata to go to (must be different from above)
-        disk_dest='/Users/user/Documents/UNITO_train_data/', # If using a RAMDisk - this is the physical save location for outputs
-        default_hyperparameters=[
-            [1e-3, 128],
-            [1e-4, 256],
-            [5e-4, 512]
-            ],
+        fcs_dir="/Users/user/Documents/final_stroke_data/flowai_fcs_files_results_qc/",  # Dir of your .fcs files
+        wsp_path="/Users/user/Documents/train_group1/StrokeIMPaCT_GatesForTraining.wsp",  # Location of you .wsp file
+        wsp_files_dir="/Users/user/Documents/train_group1/",  # Dir holding the .wsp
+        panel_meta_path="/Users/user/Documents/final_stroke_data/metadata_files/panel_metadata_all_batches.csv",  # Path to panel metadatafile needed for cycombine
+        ram_disk=False,  # Want to use a RAMDisk (MacOS only)?
+        csv_conversion_dir="/Users/user/Documents/UNITO_csv_conversion/",  # Dir for the converted files to go to
+        csv_conversion_dir_metadir="/Users/user/Documents/UNITO_csv_conversion/metadata/",  # Dir for the converstedd file metadata to go to (must be different from above)
+        disk_dest="/Users/user/Documents/UNITO_train_data/",  # If using a RAMDisk - this is the physical save location for outputs
+        default_hyperparameters=[[1e-3, 128], [1e-4, 256], [5e-4, 512]],
         problematic_gate_hyperparameters=[
             [1e-4, 16],
             [5e-6, 32],
             [1e-6, 64],
-            [1e-5, 8]
-            ],
-        problematic_gate_list=['Neutrophils', 'Non-neutrophil leukocytes'], # Must match gate labels
-        downsample_max_rows=100_000, # Downsample number of events per .fcs file to train on
-        n_worker=30,
-        device='mps' # use CPU if on Windows
+            [1e-5, 8],
+        ],
+        problematic_gate_list=[
+            "Single CellsNeutrophils",
+            "Non-neutrophil Lymphocytes",
+        ],  # Must match gate labels
+        problematic_epochs=20,
+        downsample_max_rows=100_000,  # Downsample number of events per .fcs file to train and test on
+        n_worker=32,
+        device="mps",  # use CPU if on Windows
     )
 
     # Run pipeline
     pipeline = UNITOPipeline(config)
     pipeline.run(downsample=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
